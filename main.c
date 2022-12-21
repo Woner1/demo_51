0cf29ddfb297a906def9aceecfc2a5002db99028
xinyu zeng <2609746800@qq.com>
2022-12-21 09:24:19 +0000

    函数指针

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+static void CCP_V01(void);
+static void CCP_V02(void);
+typedef void (*CCP_tpfService) (void);
+
+static void CCP_V01(void)
+{
+	printf("%s", "1");
+}
+
+static void CCP_V02(void)
+{
+	printf("%s", "2");
+}
+
+static const CCP_tpfService CCP_tpfServiceTable [2] =
+{
+	&CCP_V01,
+	&CCP_V02
+};
 
 int main()
 {
-	char a,b;
-	a=11;
-	for(char i=0; i<10; i++)
-	{
-		printf("%d \n", a+i);
-	}
-	return 0;
+	(CCP_tpfServiceTable[0]) ();
 }
